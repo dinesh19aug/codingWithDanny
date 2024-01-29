25ead9e467044f8bdc0b154956b8981818c1e7bf
dinesh19aug <dinesh19aug@gmail.com>
2024-01-28 20:30:09 -0500

    First commit to Barney book store

---
+++ src/test/java/com/javahabit/d2ccommerceapp/D2cCommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javahabit.d2ccommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2cCommerceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
