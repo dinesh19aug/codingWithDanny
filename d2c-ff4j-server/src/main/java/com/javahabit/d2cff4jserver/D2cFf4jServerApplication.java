b49d4be54c6a0523bb21d7b19e3d2571777be0f3
dinesh19aug <dinesh19aug@gmail.com>
2024-01-30 20:22:25 -0500

    Initial commit for ff4j server

---
+++ d2c-ff4j-server/src/main/java/com/javahabit/d2cff4jserver/D2cFf4jServerApplication.java
@@ -0,0 +1,13 @@
+package com.javahabit.d2cff4jserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2cFf4jServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(D2cFf4jServerApplication.class, args);
+    }
+
+}
---
+++ d2c-ff4j-server/src/test/java/com/javahabit/d2cff4jserver/D2cFf4jServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javahabit.d2cff4jserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2cFf4jServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
